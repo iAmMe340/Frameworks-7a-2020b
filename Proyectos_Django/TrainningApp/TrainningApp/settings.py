657ebd8e8bdd81f8293ac5d379cb9436fab7e3af
iAmMe340 <elkin_9078@hotmail.com>
2020-12-17 20:18:27 -0500

    heroku deploy failed

--- Proyectos_Django/TrainningApp/TrainningApp/settings.py
+++ Proyectos_Django/TrainningApp/TrainningApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import django_heroku 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,3 +161,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #busca los archivos estaticos para carga en 
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+
+django_heroku.settings(locals())
